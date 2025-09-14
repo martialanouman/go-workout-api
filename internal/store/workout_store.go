c0210d85ab4a2ec56f5f74f5810eeb3b4b51d0e3
martialanouman <80514220+martialanouman@users.noreply.github.com>
2025-09-14 19:50:03 +0000

    feat: create workout query

--- internal/store/workout_store.go
+++ internal/store/workout_store.go
@@ -1,1 +1,91 @@
 package store
+
+import "database/sql"
+
+type Workout struct {
+	Id              int64          `json:"id"`
+	Title           string         `json:"title"`
+	Description     string         `json:"description"`
+	DurationMinutes int            `json:"duration_minutes"`
+	CaloriesBurned  int            `json:"calories_burned"`
+	Entries         []WorkoutEntry `json:"entries"`
+}
+
+type WorkoutEntry struct {
+	Id              int64    `json:"id"`
+	ExerciseName    string   `json:"exercise_name"`
+	Sets            int      `json:"sets"`
+	Reps            *int     `json:"reps"`
+	DurationSeconds *int     `json:"duration_seconds"`
+	Weight          *float64 `json:"weight"`
+	Notes           string   `json:"notes"`
+	OrderIndex      int      `json:"order_index"`
+	Unit            string   `json:"unit"`
+}
+
+type WorkoutStore interface {
+	CreateWorkout(*Workout) (*Workout, error)
+	GetWorkoutById(int64) (*Workout, error)
+}
+
+type PostgresWorkoutStore struct {
+	db *sql.DB
+}
+
+func NewPostgresWorkoutStore(db *sql.DB) *PostgresWorkoutStore {
+	return &PostgresWorkoutStore{db: db}
+}
+
+func (p *PostgresWorkoutStore) CreateWorkout(workout *Workout) (*Workout, error) {
+	tx, err := p.db.Begin()
+	if err != nil {
+		return nil, err
+	}
+
+	defer tx.Rollback() // Rollback if something goes wrong
+
+	query :=
+		`INSERT INTO workouts (title, description, duration_minutes, calories_burned)
+	VALUES ($1, $2, $3, $4)
+	RETURNING id
+	`
+
+	err = tx.QueryRow(query, workout.Title, workout.Description, workout.DurationMinutes, workout.CaloriesBurned).Scan(&workout.Id)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, entry := range workout.Entries {
+		err := createWorkoutEntry(tx, workout.Id, &entry)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	err = tx.Commit()
+	if err != nil {
+		return nil, err
+	}
+
+	return workout, nil
+}
+
+func (p *PostgresWorkoutStore) GetWorkoutById(id int64) (*Workout, error) {
+	workout := &Workout{}
+	return workout, nil
+}
+
+func createWorkoutEntry(tx *sql.Tx, workoutId int64, entry *WorkoutEntry) error {
+	query :=
+		`INSERT INTO workout_entries (workout_id, exercise_name, sets, reps, duration_seconds, weight, notes, unit, order_index)
+			VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+			RETURNING id
+		`
+
+	err := tx.QueryRow(query, workoutId, entry.ExerciseName, entry.Sets, entry.Reps, entry.DurationSeconds, entry.Weight, entry.Notes, entry.Unit, entry.OrderIndex).Scan(&entry.Id)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
