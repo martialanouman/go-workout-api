0d68663cc5d4495e93617f0427256236dd749bbb
martialanouman <80514220+martialanouman@users.noreply.github.com>
2025-09-24 14:00:42 +0000

    feat: implement TokenStore interface and PostgreSQL token management

---
+++ internal/store/tokens.go
@@ -0,0 +1,55 @@
+package store
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/martialanouman/femProject/internal/tokens"
+)
+
+type TokenStore interface {
+	Insert(token *tokens.Token) error
+	CreateToken(userId int, ttl time.Duration, scope string) (*tokens.Token, error)
+	RevokeAllTokenForUser(userId int, scope string) error
+}
+
+type PostgresTokenStore struct {
+	db *sql.DB
+}
+
+func NewPostgresTokenStore(db *sql.DB) *PostgresTokenStore {
+	return &PostgresTokenStore{db}
+}
+
+func (s *PostgresTokenStore) CreateToken(userId int, ttl time.Duration, scope string) (*tokens.Token, error) {
+	token, err := tokens.GenerateToken(userId, ttl, scope)
+	if err != nil {
+		return nil, err
+	}
+
+	err = s.Insert(token)
+	return token, err
+}
+
+func (s *PostgresTokenStore) Insert(token *tokens.Token) error {
+	query := `
+		INSERT INTO tokens (hash, user_id, expiry, scope)
+		VALUES ($1, $2, $3, $4)
+	`
+
+	_, err := s.db.Exec(query, token.Hash, token.UserId, token.Expiry, token.Scope)
+
+	return err
+
+}
+
+func (s *PostgresTokenStore) RevokeAllTokenForUser(userId int, scope string) error {
+	query := `
+		DELETE FROM tokens
+		WHERE user_id = $1 AND scope = $2
+	`
+
+	_, err := s.db.Exec(query, userId, scope)
+
+	return err
+}
---
+++ internal/tokens/tokens.go
@@ -0,0 +1,40 @@
+package tokens
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
+
+const (
+	ScopeAuth = "auth"
+)
+
+type Token struct {
+	Plaintext string    `json:"token"`
+	Hash      []byte    `json:"-"`
+	UserId    int       `json:"-"`
+	Expiry    time.Time `json:"expiry"`
+	Scope     string    `json:"-"`
+}
+
+func GenerateToken(userId int, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserId: userId,
+		Expiry: time.Now().Add(ttl),
+		Scope:  scope,
+	}
+
+	emptyBytes := make([]byte, 32)
+	_, err := rand.Read(emptyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(emptyBytes)
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+
+	return token, nil
+}
